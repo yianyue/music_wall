dfb8a9dbe33d4e24abe93a6d2b591d9faeb9d1a2
yianyue <yianyue@gmail.com>
2015-07-20 19:34:36 -0400

    changed login from POST to Get, added upvote button (non-functional)

--- app/actions.rb
+++ app/actions.rb
@@ -27,6 +27,18 @@ get '/login' do
   erb :'users/login'
 end
 
+get '/login/user' do
+  @user = User.find_by(email: params[:email])
+  # TODO: display message when login fails
+  session[:user] = @user if @user && params[:password] == @user.password
+  if session[:user]
+    redirect '/songs'
+  else
+    erb :'users/login'
+  end
+end
+
+
 get '/logout' do
   session[:user] = nil
   redirect '/songs'
@@ -50,17 +62,6 @@ post '/songs' do
   end
 end
 
-post '/login' do
-  @user = User.find_by(email: params[:email])
-  # TODO: display message when login fails
-  session[:user] = @user if @user && params[:password] == @user.password
-  if session[:user]
-    redirect '/songs'
-  else
-    erb :'users/login'
-  end
-end
-
 post '/register' do
   @user = User.new(
     email: params[:email],
@@ -71,5 +72,16 @@ post '/register' do
     redirect '/songs'
   else
     erb :'users/register'
+  end
+end
+
+post 'songs/upvote' do
+  @upvote = Upvote.new
+  @upvote.user = session[:user]
+  @upvote.song = Song.find(params[:id])
+  if 
+    redirect '/songs'
+  else
+
   end
 end
