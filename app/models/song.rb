30f4482f6d158d41c1c72eaef0a4bb706e41b0a6
yianyue <yianyue@gmail.com>
2015-07-20 11:33:58 -0400

    create a songs page

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
   validates :author, :title, presence: true
   validates :url, format: {with: URI::regexp}, if: Proc.new { |a| a.url.present? }
+  # QUESTION: how doe proc work???
 end
