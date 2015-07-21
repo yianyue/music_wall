2070cbecd79d94b171b1ba5dd018370c1478e61f
yianyue <yianyue@gmail.com>
2015-07-21 18:42:26 -0400

    hide form if user has already reviewed the song. TODO: do less query

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,7 @@ class Review < ActiveRecord::Base
   belongs_to :song
 
   validates :content, presence: true
+  validates :user_id, uniqueness: { scope: :song_id, message: 'You can only review a song once' }
+
   
 end
